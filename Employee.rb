8d3272513afd55c22f9e8502c9bd863cef689c70
adavies <arielpdavies@gmail.com>
2021-05-11 13:39:08 -0400

    Added add_employees and finished bonus method

--- Employee.rb
+++ Employee.rb
@@ -1,7 +1,16 @@
-require_relative "manager.rb"
 class Employee
+    attr_reader :boss, :salary, :title
+    def initialize(name, title, salary, boss)
+        @name = name
+        @title = title
+        @salary = salary
+        @boss = boss
+    end
 
+    def bonus(multiplier)
+        bonus = @salary * multiplier
+    end
 
-
-
+    
 end
+
--- Manager.rb
+++ Manager.rb
@@ -1,8 +1,38 @@
-class Manager
+require_relative "Employee.rb"
+class Manager < Employee
+    attr_accessor :employees
 
+    def initialize(name, title, salary, boss)
+        @employees = []
+        super(name, title, salary, boss)
+    end
 
+    def bonus(multiplier)
+       total_bonus = 0
+        
+       employees.each do |employee|
+            total_bonus += employee.salary  
+       end
+       total_bonus * multiplier
+    end
+
+    def add_employees(employee)
+        employee.boss.employees << employee
+        if employee.is_a?(Manager)
+            employee.boss.employees += employee.employees
+        end
+    end
 
 
 
 
 end
+
+ned = Manager.new('ned', 'Founder', 1000000, nil)
+darren = Manager.new('Darren', 'TA Manager', 78000, ned)
+shawna = Employee.new('Shawna', 'TA', 12000, darren)
+david = Employee.new('David', 'TA', 10000, darren)
+darren.add_employees(shawna)
+darren.add_employees(david)
+ned.add_employees(darren)
+print ned.bonus(5)
